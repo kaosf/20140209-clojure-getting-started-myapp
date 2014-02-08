9732b47d641666bb08cc7df1e9cf7970db975db7
ka <ka.kaosf@gmail.com>
2014-02-09 01:05:10 +0900

    apply mylib/f to y

--- src/myapp/core.clj
+++ src/myapp/core.clj
@@ -1,11 +1,12 @@
 (ns myapp.core
   (:gen-class)
-  (:require [clojure.math.combinatorics :as combo]))
+  (:require [clojure.math.combinatorics :as combo])
+  (:require [mylib.core :refer :all]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [x y]
   (println "Hello, World!")
   (println
-    (str "x = " x ", y = " y ", xCy = "
-      (count (combo/combinations (range 0 (Integer. x)) (Integer. y))))))
+    (str "x = " x ", y = " y ", xC(y + 1) = "
+      (count (combo/combinations (range 0 (Integer. x)) (f (Integer. y)))))))
